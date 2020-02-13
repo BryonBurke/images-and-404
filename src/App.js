0ac3c451e97ff0a287b78f1e8d63a1645811e113
Student <example@example.com>
2020-02-13 12:01:03 -0800

    add header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import MySampleComponent from './components/MySampleComponent';
 import './App.css';
 import Error404 from './components/Error404';
 import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
+    <Header/>
     <Switch>
         <Route exact path='/' component={MySampleComponent} />
         <Route component={Error404} />
--- src/components/MySampleComponent.jsx
+++ src/components/MySampleComponent.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import puppy1 from '../assets/images/puppy1.png';
 import puppy2 from '../assets/images/puppy2.png';
 
-function MySampleComponent(){
+
+function MySampleComponent() {
+  const gridthing = {
+    display:'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridColumnGap: '10px',
+
+  }
+
+
+    const imageStyle = {
+      height: '50vw'
+    }
+
+
+
   return (
-    <div>
-      <img src={puppy1}/>
-      <img src={puppy2}/>
+    <div style = {gridthing}>
+      <img style = {imageStyle} src={puppy1}/>
+      <img style = {imageStyle} src={puppy2}/>
     </div>
   );
 }
